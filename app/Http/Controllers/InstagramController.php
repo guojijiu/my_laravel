130538bf95484137948f424117ba72f3f73c0165
liuwei <liuwei@rrmj.tv>
2018-07-30 11:59:42 +0800

    添加测试账号

--- app/Http/Controllers/InstagramController.php
+++ app/Http/Controllers/InstagramController.php
@@ -11,8 +11,8 @@ class InstagramController
     {
         //允许使用web进行接口调用
         \InstagramAPI\Instagram::$allowDangerousWebUsageAtMyOwnRisk = true;
-        $username = '11';
-        $password = '22';
+        $username = 'MrXinrain';
+        $password = 'a5711947';
         $ig = new Instagram(true, false);
         try {
             $ig->login($username, $password);
