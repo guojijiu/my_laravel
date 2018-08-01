29a8b9c856bf748c19172937894e3b54d8375cae
liuwei <liuwei@rrmj.tv>
2018-08-01 11:33:47 +0800

    ins异常错误修改

--- app/Http/Controllers/InstagramController.php
+++ app/Http/Controllers/InstagramController.php
@@ -9,10 +9,8 @@ class InstagramController
     public function login()
     {
         try {
-            $instagram = Instagram::withCredentials('MrXinrain', 'a5711947');
-            $instagram->login();
-            $account = $instagram->getAccountById(3);
-            echo $account->getUsername();
+            $ig = new Instagram('5469307698.1677ed0.ebdbe1794aad4e7aab2c4e378a44c001');
+            return $ig->media();
         } catch (\Exception $e) {
             throw new \Exception($e->getMessage());
         }
