e844ac4274056fa3497d9865907320313d54988a
liuwei <liuwei@rrmj.tv>
2018-10-12 18:10:32 +0800

    获取url链接

--- app/Model/Star.php
+++ app/Model/Star.php
@@ -6,9 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Star extends Model
 {
-    protected $fillable = [
-        'name', 'sex', 'korea_name', 'birthday', 'constellation', 'avater', 'country', 'detail',
-        'description', 'social_account', 'fans', 'status', 'created_at'
-    ];
+
 
 }
